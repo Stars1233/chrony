Stars1233/chrony

commit 866bedef0b648fdd6cc68f8cb4d75e4303244c76

main: add -N option for s6-style service readiness notification
In this notification scheme a service supervisor creates a pipe
and passes the write file descriptor to chronyd. When chronyd is
initialised it writes a newline to the descriptor and the
supervisor is informed that chronyd has successfully started and
is in a ready state.

https://skarnet.org/software/s6/notifywhenup.html

main.c
@@ -70,6 +70,8 @@ static int exit_status = 0;
 
 static int reload = 0;
 
+static int notify_fd = 0;
+
 static REF_Mode ref_mode = REF_ModeNormal;
 
 /* ================================================== */
@@ -110,6 +112,14 @@ delete_pidfile(void)
 static void
 notify_system_manager(int start)
 {
+  /* s6-style ready notification using a pipe from the parent */
+  if (start && notify_fd) {
+    if (write(notify_fd, "\n", 1) != 1)
+      LOG_FATAL("Could not send notification requested by -N option");
+    close(notify_fd);
+    notify_fd = 0;
+  }
+
 #ifdef LINUX
   /* The systemd protocol is documented in the sd_notify(3) man page */
   const char *message, *path = getenv("NOTIFY_SOCKET");
@@ -454,6 +464,7 @@ print_help(const char *progname)
              "  -P PRIORITY\tSet process priority (0)\n"
              "  -m\t\tLock memory\n"
              "  -x\t\tDon't control clock\n"
+             "  -N FD\t\tNotify supervisor\n"
              "  -v, --version\tPrint version and exit\n"
              "  -h, --help\tPrint usage and exit\n",
              progname, DEFAULT_CONF_FILE, DEFAULT_USER);
@@ -512,7 +523,7 @@ int main
   optind = 1;
 
   /* Parse short command-line options */
-  while ((opt = getopt(argc, argv, "46df:F:hl:L:mnpP:qQrRst:u:Uvx")) != -1) {
+  while ((opt = getopt(argc, argv, "46df:F:hl:L:mnN:pP:qQrRst:u:Uvx")) != -1) {
     switch (opt) {
       case '4':
       case '6':
@@ -540,6 +551,9 @@ int main
         break;
       case 'n':
         nofork = 1;
+        break;
+      case 'N':
+        notify_fd = parse_int_arg(optarg);
         break;
       case 'p':
         print_config = 1;

commit dbd4775a5b62cefd8a5c00887d5d98ddf54bd567

main: don't close -N descriptor after forking
While this is not the expected use case, avoid closing the s6-style
notification file descriptor specified by the -N option after forking to
make it work even without the -d or -n option.

Fixes: 866bedef0b64 ("main: add -N option for s6-style service readiness notification")

main.c
@@ -70,7 +70,7 @@ static int exit_status = 0;
 
 static int reload = 0;
 
-static int notify_fd = 0;
+static int notify_fd = -1;
 
 static REF_Mode ref_mode = REF_ModeNormal;
 
@@ -113,11 +113,11 @@ static void
 notify_system_manager(int start)
 {
   /* s6-style ready notification using a pipe from the parent */
-  if (start && notify_fd) {
+  if (start && notify_fd >= 0) {
     if (write(notify_fd, "\n", 1) != 1)
       LOG_FATAL("Could not send notification requested by -N option");
     close(notify_fd);
-    notify_fd = 0;
+    notify_fd = -1;
   }
 
 #ifdef LINUX
@@ -416,9 +416,9 @@ go_daemon(void)
       }
 
       /* Don't keep stdin/out/err from before. But don't close
-         the parent pipe yet, or reusable file descriptors. */
+         the parent pipe, notification, and reusable file descriptors. */
       for (fd=0; fd<1024; fd++) {
-        if (fd != pipefd[1] && !SCK_IsReusable(fd))
+        if (fd != pipefd[1] && fd != notify_fd && !SCK_IsReusable(fd))
           close(fd);
       }
 